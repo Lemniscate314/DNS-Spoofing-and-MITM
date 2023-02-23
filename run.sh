455dfde4d08793a86839ddf8a471884d22066622
Issa Mboup <issa.mboup314@gmail.com>
2023-02-23 13:21:55 +0000

    script run

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#sudo kill $(ps aux | grep "python .*httpServer.py" | awk '{print $2}')
-#sudo kill $(ps aux | grep "python .*dnsServer.py" | awk '{print $2}')
-#sudo kill $(ps aux | grep "python .*client.py" | awk '{print $2}')
+sudo kill $(ps aux | grep "python .*httpServer.py" | awk '{print $2}')
+sudo kill $(ps aux | grep "python .*dnsServer.py" | awk '{print $2}')
+sudo kill $(ps aux | grep "python .*client.py" | awk '{print $2}')
 
 sudo python3 dnsServer/dnsServer.py > log/dnsOut.txt &
 sudo python3 httpServer/httpServer.py > log/httpserverOut.txt &
